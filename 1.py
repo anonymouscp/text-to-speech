8f4a437ec121dc601fe5d448ff446cb873b62f7c
anonymouscp <48428859+anonymouscp@users.noreply.github.com>
2020-11-05 22:53:12 +0530

    Update 1.py
    Hii The previous code was having some errors.

--- 1.py
+++ 1.py
@@ -1,13 +1,40 @@
-from gtts import gTTS
+#Credit: Original owner is geeksforgeeks
+# Import the required module for text  
+# to speech conversion 
 
-import os
+from gtts import gTTS 
 
-f=open('1.txt')
-x=f.read()
+  
+# This module is imported so that we can  
+# play the converted audio 
 
-language='en'
+import os 
 
-audio=gTTS(text=x,lang=language,slow=False)
+  
+# The text that you want to convert to audio 
 
-audio.save("1.wav")
-os.system("1.wav")
+mytext = 'Welcome to this journey of learning'
+
+  
+# Language in which you want to convert 
+
+language = 'en'
+
+  
+# Passing the text and language to the engine,  
+# here we have marked slow=False. Which tells  
+# the module that the converted audio should  
+# have a high speed 
+
+myobj = gTTS(text=mytext, lang=language, slow=False) 
+
+  
+# Saving the converted audio in a mp3 file named 
+# welcome  
+
+myobj.save("welcome.mp3") 
+
+  
+# Playing the converted file 
+
+os.system("mpg321 welcome.mp3")
